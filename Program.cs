f047df5b8dc98237b76d5d2f96e017ee74deb90a
noeking23 <noecarlosmanamiespinoza@gmail.com>
2023-11-22 10:59:26 -0400

    Libretia Online

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MatematicasLib {
+    public static class Calculadora {
+        public static double Sumar(double a, double b) {
+            return a + b;
+        }
+
+        public static double Restar(double a, double b) {
+            return a - b;
+        }
+
+        public static double Multiplicar(double a, double b) {
+            return a * b;
+        }
+
+        public static double Dividir(double a, double b) {
+            if (b == 0) {
+                throw new DivideByZeroException("No se puede dividir por cero.");
+            }
+            return a / b;
+        }
+
+        public static double ElevarAlCuadrado(double a) {
+            return a * a;
+        }
+
+        public static double RaizCuadrada(double a) {
+            if (a < 0) {
+                throw new ArgumentException("No se puede calcular la raíz cuadrada de un número negativo.");
+            }
+            return Math.Sqrt(a);
+        }
+
+        public static double Potencia(double baseNum, double exponente) {
+            return Math.Pow(baseNum, exponente);
+        }
+
+        public static double Modulo(double a, double b) {
+            return a % b;
+        }
+    }
+}
